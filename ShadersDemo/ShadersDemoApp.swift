46518fbcc25f2711c98b770a335177afdee7c2ec
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-07-21 13:45:53 +0300

    Initial Commit

---
+++ ShadersDemo/ShadersDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadersDemoApp.swift
+//  ShadersDemo
+//
+//  Created by Vasichko Anna on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShadersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
